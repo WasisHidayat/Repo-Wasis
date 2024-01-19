cc25359c79302721e6092e42dc87da4e8bf87a68
1722 <1722@LAPTOP-OM51GH8M>
2024-01-19 16:12:03 +0700

    aaa

--- Test Suites/Remove Product.ts
+++ Test Suites/Remove Product.ts
@@ -11,11 +11,4 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>a6065571-8ab7-4dec-bfc4-22ca0348ec8d</testSuiteGuid>
-   <testCaseLink>
-      <guid>5c6be103-cce9-4e56-b8a4-c1ec644504c6</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Remove Procuct</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
 </TestSuiteEntity>
