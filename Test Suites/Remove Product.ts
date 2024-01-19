9ee6e347f55391133d18ed29fae29ce415bb64f7
1722 <1722@LAPTOP-OM51GH8M>
2024-01-19 15:39:36 +0700

    hh

--- Test Suites/Remove Product.ts
+++ Test Suites/Remove Product.ts
@@ -5,10 +5,17 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>0</numberOfRerun>
+   <numberOfRerun>3</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>false</rerunImmediately>
+   <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>a6065571-8ab7-4dec-bfc4-22ca0348ec8d</testSuiteGuid>
+   <testCaseLink>
+      <guid>5c6be103-cce9-4e56-b8a4-c1ec644504c6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Remove Procuct</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
