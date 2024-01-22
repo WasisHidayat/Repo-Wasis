dac59c17a024e9358986e791c7c3e023b70bf412
1722 <1722@LAPTOP-OM51GH8M>
2024-01-22 09:54:17 +0700

    Test Suite Exe 2

--- Test Suites/Sauce Demo Project.ts
+++ Test Suites/Sauce Demo Project.ts
@@ -4,7 +4,7 @@
    <name>Sauce Demo Project</name>
    <tag></tag>
    <delayBetweenInstances>0</delayBetweenInstances>
-   <executionMode>PARALLEL</executionMode>
+   <executionMode>SEQUENTIAL</executionMode>
    <maxConcurrentInstances>8</maxConcurrentInstances>
    <testSuiteRunConfigurations>
       <TestSuiteRunConfiguration>
@@ -21,8 +21,26 @@
          <configuration>
             <groupName>Web Desktop</groupName>
             <profileName>default</profileName>
-            <requireConfigurationData>false</requireConfigurationData>
-            <runConfigurationId>Chrome</runConfigurationId>
+            <requireConfigurationData>true</requireConfigurationData>
+            <runConfigurationData>
+               <entry>
+                  <key>testCloudMobile</key>
+                  <value>{
+  &quot;id&quot;: &quot;samsung_galaxy_tab_s9_plus&quot;,
+  &quot;isTablet&quot;: false,
+  &quot;manufacturer&quot;: [
+    &quot;Samsung&quot;
+  ],
+  &quot;name&quot;: &quot;Samsung Galaxy Tab S9 Plus&quot;,
+  &quot;osVersion&quot;: &quot;13&quot;,
+  &quot;mobileDeviceId&quot;: &quot;Samsung_Galaxy_Tab_S9_Plus_real_us&quot;,
+  &quot;os&quot;: &quot;Android&quot;,
+  &quot;timeout&quot;: 15,
+  &quot;usingTunnel&quot;: false
+}</value>
+               </entry>
+            </runConfigurationData>
+            <runConfigurationId>TestCloud</runConfigurationId>
          </configuration>
          <runEnabled>true</runEnabled>
          <testSuiteEntity>Test Suites/Checkout</testSuiteEntity>
