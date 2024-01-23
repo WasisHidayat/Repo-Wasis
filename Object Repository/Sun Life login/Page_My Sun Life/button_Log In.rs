57fe6763c159ab4945f30b2203a9265d3a61def0
1722 <1722@LAPTOP-OM51GH8M>
2024-01-23 13:51:39 +0700

    Commit new sun life

---
+++ Object Repository/Sun Life login/Page_My Sun Life/button_Log In.rs
@@ -0,0 +1,91 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>button_Log In</name>
+   <tag></tag>
+   <elementGuidId>faaabe76-bc05-4c3d-adb3-dffbbbd693b8</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>(//button[@type='button'])[4]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>button.btn.btn-primary.sl-btn.w-100.my-4.d-flex</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>button</value>
+      <webElementGuid>9f8b950b-dec3-4f64-9485-ac90a59b7a3b</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>type</name>
+      <type>Main</type>
+      <value>button</value>
+      <webElementGuid>ecf12e64-d6f7-468e-9e15-8e2555616950</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>btn btn-primary sl-btn w-100 my-4 d-flex</value>
+      <webElementGuid>20f2fdd5-8c91-4958-a0ee-c38623f776ea</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Log In</value>
+      <webElementGuid>939bc43f-e8d2-4f49-bedb-60dfa60bd5f4</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;root&quot;)/div[@class=&quot;d-flex flex-column justify-content-between h-100&quot;]/div[@class=&quot;position-relative pb-5&quot;]/div[@class=&quot;position-relative h-100 mb-4&quot;]/div[@class=&quot;position-relative z-2&quot;]/div[@class=&quot;container px-0&quot;]/div[@class=&quot;d-flex align-items-top align-items-sm-center bg-grey-50 p-4 max-w-368 min-h-400&quot;]/div[@class=&quot;w-100&quot;]/button[@class=&quot;btn btn-primary sl-btn w-100 my-4 d-flex&quot;]</value>
+      <webElementGuid>d56de4b8-fd13-4297-9e53-fde6ebe2ffd0</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:attributes</name>
+      <type>Main</type>
+      <value>(//button[@type='button'])[4]</value>
+      <webElementGuid>ce1baa7b-7495-4384-ac43-2b569006e3c5</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='root']/div[3]/div/div/div[2]/div/div/div/button</value>
+      <webElementGuid>c38f85ea-6670-4917-96cf-f12f5b8b3933</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div/div/div[2]/div/div/div/button</value>
+      <webElementGuid>3d2429bf-9faf-4024-93b8-d8e6943b8496</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//button[@type = 'button' and (text() = 'Log In' or . = 'Log In')]</value>
+      <webElementGuid>6402bb92-9b9d-45ec-b367-55b353407686</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
--- Test Suites/Sun Life Suite/Sun life login.ts
+++ Test Suites/Sun Life Suite/Sun life login.ts
@@ -12,7 +12,7 @@
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>da2155d9-78fc-48d0-8574-0bdb97975f42</testSuiteGuid>
    <testCaseLink>
-      <guid>99590996-60e6-4e4c-9298-d5ab6f4a1743</guid>
+      <guid>c6187e78-2e9d-4416-8260-1a314ff5a3d6</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Sun Life/Login</testCaseId>
