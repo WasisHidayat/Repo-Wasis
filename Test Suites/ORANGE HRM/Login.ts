0f7d5cdb095c591d4b7c8ccb90b3a1bb0038abcd
1722 <1722@LAPTOP-OM51GH8M>
2024-01-25 09:59:10 +0700

    Commit new test case

---
+++ Test Suites/ORANGE HRM/Login.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>a192095c-8cbf-4f6e-ae45-879d1d00d58c</testSuiteGuid>
+   <testCaseLink>
+      <guid>3b130be1-425a-4ace-8ec5-e72f954786b2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ORANGE HRM/Login</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
